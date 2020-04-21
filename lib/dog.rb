09c2f30491a305caac06c6cb474c296e1c0535ff
Thomas John Helin <thomashelin@yahoo.com>
2020-04-21 22:41:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,14 @@
 class Dog
-  def @name=(dog_name)
+  def @name::(dog_name)
     this_dogs_name = dog_name
   end
 
   def name
     this_dogs_name
   end
-  lassie = Dog.new
 end
-end
+
+lassie = Dog.new
+lassie.name= "Lassie"
+
+puts lassie.name
